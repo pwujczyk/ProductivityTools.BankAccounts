d2ccd703eabd945c668a04c43efa1e919dc8a9b8
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-09-23 07:51:24 +0200

    test call

---
+++ ProductivityTools.BankAccounts.Api/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace ProductivityTools.BankAccounts.Api.Controllers
+{
+    [ApiController]
+    [Route("Test")]
+    public class TestController : Controller
+    {
+        [HttpPost("Hi")]
+        public string Index(object str)
+        {
+            JObject json = JObject.Parse(str.ToString());
+            string r = $"Hello {json["name"]}";
+            return r;
+        }
+    }
+}

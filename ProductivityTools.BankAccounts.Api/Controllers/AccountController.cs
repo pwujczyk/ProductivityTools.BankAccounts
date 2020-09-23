6f751de6651a4daf2b101af3f897c60b6230848c
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-09-23 08:00:31 +0200

    account controller

---
+++ ProductivityTools.BankAccounts.Api/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProductivityTools.Bank.Millenium.Objects;
+
+namespace ProductivityTools.BankAccounts.Api.Controllers
+{
+    [ApiController]
+    [Route("Account")]
+    public class AccountController : Controller
+    {
+        [HttpPost("BasicData")]
+        public IActionResult BasicData(BasicData basicData)
+        {
+            return View();
+        }
+    }
+}

61f4aeb60e8636c1225bda8597d974694f31bbef
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-09-25 15:51:16 +0200

    endpionts

--- ProductivityTools.BankAccounts.Api/Controllers/AccountController.cs
+++ ProductivityTools.BankAccounts.Api/Controllers/AccountController.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using ProductivityTools.BankAccounts.Commands;
 using ProductivityTools.BankAccounts.Contract;
 
 namespace ProductivityTools.BankAccounts.Api.Controllers
 {
     [ApiController]
-    [Route("Account")]
+    [Route(EndpointNames.AccountControllerName)]
     public class AccountController : Controller
     {
         private readonly IBMCommands Commands;
@@ -19,10 +20,18 @@ namespace ProductivityTools.BankAccounts.Api.Controllers
             this.Commands = commands;
         }
 
-        [HttpPost("BasicData")]
+        [HttpPost(EndpointNames.BasicDataMethodName)]
         public IActionResult BasicData(BasicData basicData)
         {
             this.Commands.SaveBasicData(basicData);
+            return Ok();
+        }
+
+
+        [HttpPost(EndpointNames.SaveTransactionsMethodName)]
+        public IActionResult SaveTransactions(List<Transaction> transactions)
+        {
+            this.Commands.SaveTransactions(transactions);
             return Ok();
         }
     }
